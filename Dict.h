6d913004e105991240d02a9be1b1858f90c8b261
LKCOPTUD <lkcoptud@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2025-01-08 21:31:58 +0100

    Añadida interfaz genérica Dict modificada

--- Dict.h
+++ Dict.h
@@ -1,20 +1,14 @@
 #ifndef DICT_H
 #define DICT_H
-
 #include <string>
-#include <stdexcept>
 
-// Clase abstracta pura y genérica Dict<V>
-template <typename V>
-class Dict {
+template<typename V> class Dict {
 	public:
-    		virtual ~Dict() = default;
-
-    		// Métodos virtuales puros
-    		virtual void insert(std::string key, V value) = 0;
-    		virtual V search(std::string key) = 0;
-    		virtual V remove(std::string key) = 0;
-    		virtual int entries() = 0;
+		virtual void insert(std::string key, V value);
+		virtual V search(std::string key);
+		virtual V remove(std::string key);
+		virtual int entries();
 };
+
 #endif
 
