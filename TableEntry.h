292a4a1f662514e126eb401f417412b79249b443
LKCOPTUD <lkcoptud@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2025-01-08 21:35:24 +0100

    Añadida implementación de la clase TableEntry modificada

--- TableEntry.h
+++ TableEntry.h
@@ -1,38 +1,42 @@
 #ifndef TABLEENTRY_H
 #define TABLEENTRY_H
-#include <string>
+
 #include <ostream>
+#include <string>
 
-// Clase genérica TableEntry<V>
-template <typename V>
-class TableEntry {
+template<typename V> class TableEntry {
 	public:
-    		std::string key; // Clave del par
-    		V value; // Valor del par
+		std::string key;
+		V value;
+		TableEntry(std::string key, V value){
+			this->key = key;
+			this->value = value;
+		};
+		TableEntry(std::string key){
+			this->key = key;
+		};
+		TableEntry(){
+			this->key = " ";
+		};
+		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key == te2.key){
+				return true;
+			}
+			return false;
+		};
+		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+			if(te1.key != te2.key){
+				return true;
+			}
+			return false;
+		};
+		friend std::ostream&operator<<(std::ostream &out, const TableEntry<V> &te){
+			out << "Clave: " << te.key;
+			out ", Valor: " << te.value;
+			return out;
+		};
 
-    		// Constructor con el par clave->valor
-    		TableEntry(std::string key, V value) : key(key), value(value) {}
-
-    		// Constructor solo con clave (sin valor)
-    		TableEntry(std::string key) : key(key) {}
-
-    		// Constructor con clave vacía y sin valor
-    		TableEntry() : key(""), value(V()) {}
-
-    		// Sobrecarga global del operador ==
-    		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-        		return te1.key == te2.key;
-    		}
-
-    		// Sobrecarga global del operador !=
-    		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
-        		return !(te1 == te2);
-    		}
-
-    		// Sobrecarga global del operador <<
-    		friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
-        		out << "Clave: " << te.key << ", Valor: " << te.value;
-        		return out;
-    		}
 };
+
 #endif
+
