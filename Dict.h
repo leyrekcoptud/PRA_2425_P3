d50a8e121d15cc2884d1228648400e6281a51dde
LKCOPTUD <lkcoptud@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2025-01-09 01:18:12 +0100

    Añadida interfaz genérica Dict

--- Dict.h
+++ Dict.h
@@ -1,14 +1,28 @@
 #ifndef DICT_H
 #define DICT_H
+
+#include <stdexcept>
 #include <string>
 
-template<typename V> class Dict {
-	public:
-		virtual void insert(std::string key, V value);
-		virtual V search(std::string key);
-		virtual V remove(std::string key);
-		virtual int entries();
+// Clase abstracta pura y genérica Dict<V>
+template <typename V>
+class Dict {
+public:
+    // Destructor virtual
+    virtual ~Dict() {}
+
+    // Método para insertar un par clave-valor en el diccionario
+    virtual void insert(std::string key, V value) = 0;
+
+    // Método para buscar un valor correspondiente a una clave en el diccionario
+    virtual V search(std::string key) = 0;
+
+    // Método para eliminar un par clave-valor del diccionario
+    virtual V remove(std::string key) = 0;
+
+    // Método para obtener el número de elementos en el diccionario
+    virtual int entries() = 0;
 };
 
-#endif
+#endif // DICT_H
 
