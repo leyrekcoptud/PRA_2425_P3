f4e9761624c9f485f3660a4920ad06652aa4bda5
LKCOPTUD <lkcoptud@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2025-01-09 01:31:05 +0100

    Añadida implementación de la clase HashTable

--- HashTable.h
+++ HashTable.h
@@ -1,90 +1,107 @@
-#ifdef HASHTABLE_H
+#ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-#include "/mnt/c/Users/Usuario/Desktop/Programacion_segundo/Pro_avanzada/Practica_1/ListLinked.h"
-#include "Dict.h"
-#include "TableEntry.h"
-#include <ostream>
 #include <stdexcept>
-#include <iostream>
 #include <string>
+#include <iostream>
+#include "../../P1/PRA_2425_P1/ListLinked.h"
+#include "TableEntry.h"
+#include "Dict.h"
 
-typename<typename V>
-class HashTable<V> : Dict<V> {
-	private:
-		int n;
-		int max;
-		ListLinked<TableEntry<V>> *table;
-		int h(std::string key){
-			int sum = 0;
-			size_str = key.size();
-			for( int i = 1; i <= size_str; ++i){
-				char aux = key.at(i)
-				sum = sum + std:string::int(aux);
-			}
-			pos = sum % max;
-			return pos;
-		};
-	public:
-		HashTable(int size){
-			n = 0;
-			max = size;
-			table = new ListLinked<TableEntry<V>>[max];
-		};
-		~HashTable(){
-			detele[] table;;
-		};
-		int capacity(){
-			return max;
-		};
+template <typename V>
+class HashTable : public Dict<V> {
+private:
+    int n; // Número de elementos
+    int max; // Tamaño de la tabla hash
+    ListLinked<TableEntry<V>>* table; // Tabla de cubetas
 
-		friend std::ostream&operator<<(std::ostream out, const HashTable<V> &th){
-			out << "Contenido de las cubetas de la tabla: \n";
-			for(int i = 0;i < th.max; i++){
-				out << th.table[i] << "\n";
-			}
-			return out
-		};
-		V operator[](std::string key){
-			int pos = h(key);
-			confir = table[pos].search(TableEntry<V>(key)); // Search para vee si existe la clave // 
-			if(confir == -1){
-				throw std::runtime_error("LLave no encontrada")
-			}
-			return table[pos][confir].value;
-		};
-		void insert(std::string key, V value) override{
-			int pos = h(key);
-			confir = table[pos].search(TableEntry<V>(key));
-			if(confir != -1){
-				throw std::runtime_error("La llave ya existe");
-			}
-			TableEntry<V> Cubeta_nueva(key, value);
-			table[pos].prepend(Cubeta_nueva);
-			n++;
-		};
-		V search(std::string key) override{
-			int pos = h(key);
-			confir = table[pos].search(TableEntry<V>(key));
-			if(confir == -1){
-				throw std::runtime_error("llave no encontrada");
-			}
-			return confir;
-		};
-		V remove(std::string key) override{
-			int pos = h(key);
-			confir = table[pos].search(TableEntry<V>(key));
-			if(confir == -1){
-				throw std::runtime_error;
-			}
-			V data_eliminada = table[pos][confir].value;
-			table[pos].remove();
-			n--;
-			return data_eliminada;
-		};
-		int entries() override{
-			return n;
-		};
+    // Función hash
+    int h(std::string key) {
+        int sum = 0;
+        for (char c : key) {
+            sum += static_cast<int>(c);
+        }
+        return sum % max;
+    }
+
+public:
+    // Constructor
+    HashTable(int size) : n(0), max(size) {
+        table = new ListLinked<TableEntry<V>>[max];
+    }
+
+    // Destructor
+    ~HashTable() {
+        delete[] table;
+    }
+
+    // Método insert
+    void insert(std::string key, V value) override {
+        int index = h(key);
+        TableEntry<V> entry(key, value);
+        for (int i = 0; i < table[index].size(); ++i) {
+            if (table[index].get(i) == entry) {
+                throw std::runtime_error("Key already exists in the hash table.");
+            }
+        }
+        table[index].insert_end(entry);
+        ++n;
+    }
+
+    // Método search
+    V search(std::string key) override {
+        int index = h(key);
+        TableEntry<V> entry(key);
+        for (int i = 0; i < table[index].size(); ++i) {
+            if (table[index].get(i) == entry) {
+                return table[index].get(i).value;
+            }
+        }
+        throw std::runtime_error("Key not found in the hash table.");
+    }
+
+    // Método remove
+    V remove(std::string key) override {
+        int index = h(key);
+        TableEntry<V> entry(key);
+        for (int i = 0; i < table[index].size(); ++i) {
+            if (table[index].get(i) == entry) {
+                V value = table[index].get(i).value;
+                table[index].remove(i);
+                --n;
+                return value;
+            }
+        }
+        throw std::runtime_error("Key not found in the hash table.");
+    }
+
+    // Método entries
+    int entries() override {
+        return n;
+    }
+
+    // Método capacity
+    int capacity() {
+        return max;
+    }
+
+    // Sobrecarga del operador []
+    V operator[](std::string key) {
+        return search(key);
+    }
+
+    // Sobrecarga del operador <<
+    friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &ht) {
+        for (int i = 0; i < ht.max; ++i) {
+            out << "Cubeta " << i << ": ";
+            for (int j = 0; j < ht.table[i].size(); ++j) {
+                out << ht.table[i].get(j) << " ";
+            }
+            out << std::endl;
+        }
+        return out;
+    }
 };
 
-#endif
+#endif // HASHTABLE_H
+
