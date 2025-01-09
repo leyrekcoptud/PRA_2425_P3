794ce34fe02567dc89f3d04e80e2217bc3d6a5ce
LKCOPTUD <lkcoptud@alumno.upv.es@ldsic-vdi18.upvnet.upv.es>
2025-01-09 01:20:25 +0100

    Añadida implementación de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -1,41 +1,41 @@
 #ifndef TABLEENTRY_H
 #define TABLEENTRY_H
 
-#include <ostream>
+#include <iostream>
 #include <string>
 
-template<typename V> class TableEntry {
-	public:
-		std::string key;
-		V value;
-		TableEntry(std::string key, V value){
-			this->key = key;
-			this->value = value;
-		};
-		TableEntry(std::string key){
-			this->key = key;
-		};
-		TableEntry(){
-			this->key = " ";
-		};
-		friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
-			if(te1.key == te2.key){
-				return true;
-			}
-			return false;
-		};
-		friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
-			if(te1.key != te2.key){
-				return true;
-			}
-			return false;
-		};
-		friend std::ostream&operator<<(std::ostream &out, const TableEntry<V> &te){
-			out << "('" << te.key << "' => " << te.value << ")";
-			return out;
-		};
+// Clase genérica TableEntry<V> para pares clave-valor
+template <typename V>
+class TableEntry {
+public:
+    std::string key; // Clave del par
+    V value;         // Valor del par
 
+    // Constructor con clave y valor
+    TableEntry(std::string key, V value) : key(key), value(value) {}
+
+    // Constructor solo con clave
+    TableEntry(std::string key) : key(key), value(V()) {}
+
+    // Constructor por defecto
+    TableEntry() : key(""), value(V()) {}
+
+    // Sobrecarga del operador ==
+    friend bool operator==(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return te1.key == te2.key;
+    }
+
+    // Sobrecarga del operador !=
+    friend bool operator!=(const TableEntry<V>& te1, const TableEntry<V>& te2) {
+        return !(te1 == te2);
+    }
+
+    // Sobrecarga del operador <<
+    friend std::ostream& operator<<(std::ostream& out, const TableEntry<V>& te) {
+        out << te.key << " -> " << te.value;
+        return out;
+    }
 };
 
-#endif
+#endif // TABLEENTRY_H
 
